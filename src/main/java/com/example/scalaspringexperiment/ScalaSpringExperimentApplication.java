a144853806d60cef70ce7ea7c1bc1e6503f40aa8
Nick Fellows <halfhp@gmail.com>
2021-09-07 18:40:08 -0500

    pristine spring boot project

---
+++ src/main/java/com/example/scalaspringexperiment/ScalaSpringExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.scalaspringexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalaSpringExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalaSpringExperimentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scalaspringexperiment/ScalaSpringExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scalaspringexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalaSpringExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
