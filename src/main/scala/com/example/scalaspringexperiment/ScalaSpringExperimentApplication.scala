573087bf17a8f36b6e4bb6c169c4478a09205aca
Nick Fellows <halfhp@gmail.com>
2021-09-07 22:22:54 -0500

    wrap app with zio

--- src/main/scala/com/example/scalaspringexperiment/ScalaSpringExperimentApplication.scala
+++ src/main/scala/com/example/scalaspringexperiment/ScalaSpringExperimentApplication.scala
@@ -1,14 +1,26 @@
 package com.example.scalaspringexperiment
 
-import org.springframework.boot.SpringApplication
+import org.springframework.boot.{ExitCodeGenerator, SpringApplication}
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.event.ContextClosedEvent
+import org.springframework.context.{ApplicationEvent, ApplicationListener}
+import zio.Task
+import zio.ZIO
+import zio.IO
 
 @SpringBootApplication
 class ScalaSpringExperimentApplication
 
-object ScalaSpringExperimentApplication {
+object ScalaSpringExperimentApplication extends zio.App {
 
-  def main(args: Array[String]): Unit = {
-    SpringApplication.run(classOf[ScalaSpringExperimentApplication])
+  def run(args: List[String]) = {
+    IO.effectAsync { cb =>
+      val app = SpringApplication.run(classOf[ScalaSpringExperimentApplication])
+      val listener = new ApplicationListener[ContextClosedEvent] {
+        override def onApplicationEvent(event: ContextClosedEvent) = cb(IO.unit)
+      }
+      app.addApplicationListener(listener)
+    }.exitCode
   }
+
 }
