03b449e741ce12c2d982d5ffaddcd6b8e86f0aad
Nick Fellows <halfhp@gmail.com>
2025-04-22 13:47:00 -0500

    persistence insert values are now abstracted

--- src/main/scala/com/example/scalaspringexperiment/service/FooService.scala
+++ src/main/scala/com/example/scalaspringexperiment/service/FooService.scala
@@ -3,8 +3,6 @@ package com.example.scalaspringexperiment.service
 import cats.implicits.*
 import com.example.scalaspringexperiment.model.FooDomain
 import com.example.scalaspringexperiment.dao.{Persistence, PersistenceLayer, TableInfo}
-import doobie.*
-import doobie.implicits.*
 import org.springframework.stereotype.Service
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,8 +15,6 @@ class FooService(
 
   override val logger = LoggerFactory.getLogger(classOf[FooService])
   override val tableInfo: TableInfo[FooDomain] = TableInfo.build[FooDomain]()
-
-  override def insertValues(model: FooDomain) = fr"${model.dateCreated},${model.lastUpdated},${model.a},${model.b}"
 
   case class FooServiceError(
     message: String
