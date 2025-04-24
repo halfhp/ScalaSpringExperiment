23d98f1f079f8e5c056eb7d0052d641e973b800c
Nick Fellows <halfhp@gmail.com>
2025-04-23 19:51:48 -0500

    basic webflux implementation

--- src/main/scala/com/example/scalaspringexperiment/controller/SimpleController.scala
+++ src/main/scala/com/example/scalaspringexperiment/controller/SimpleController.scala
@@ -8,7 +8,7 @@ import doobie.implicits.*
 import io.circe.*
 import io.circe.generic.auto.*
 import io.circe.syntax.*
-import com.example.scalaspringexperiment.util.MyJsonCodecs.*
+import com.example.scalaspringexperiment.util.MyJsonCodecs.timestampCodec
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
