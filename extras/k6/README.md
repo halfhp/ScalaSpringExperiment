cc7b1dde950ee8a9a3db2ee448e8cbe0f6b04fd2
Nick Fellows <halfhp@gmail.com>
2025-05-04 18:21:22 -0500

    adds k6 benchmarking

---
+++ extras/k6/README.md
@@ -0,0 +1,5 @@
+# Benchmarking With K6
+This extra provides a simple docker-compose environment that can be used to benchmark
+an instance of the spring app running on localhost.
+
+Upon running docker-compose-k6.yml, navigate to [http://localhost:3000](http://localhost:3000) to view the k6 dashboard.
---
+++ extras/k6/test/test.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    vus: 10,
+    duration: '30s',
+};
+
+export default function () {
+    const res = http.get('http://host.docker.internal:8080');
+    check(res, { 'status is 200': (r) => r.status === 200 });
+    sleep(1);
+}
